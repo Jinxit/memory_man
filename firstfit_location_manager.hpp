e08d386fcd207ed03efe65e8504f702751f77ecd
Lucas Åström <lucasas@kth.se>
2015-07-01 12:50:34 +0200

    added class for first fit allocation with memory location sorting

---
+++ firstfit_location_manager.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "firstfit_manager.hpp"
+
+class firstfit_location_manager : public firstfit_manager
+{
+protected:
+	virtual void coalesce()
+	{
+		for (auto it_a = free_blocks.begin(); it_a != free_blocks.end(); ++it_a)
+		{
+			for (auto it_b = it_a; it_b != free_blocks.end(); ++it_b)
+			{
+				if (it_a == it_b)
+					continue;
+
+				if (it_a->start + it_a->size == it_b->start)
+				{
+					free_blocks.emplace_hint(it_a, it_a->start, it_a->size + it_b->size);
+					free_blocks.erase(it_a);
+					free_blocks.erase(it_b);
+					++it_a;
+					break;
+				}
+			}
+		}
+	}
+
+	static bool compare(const block& a, const block& b)
+	{
+		return a.start < b.start;
+	}
+
+public:
+	firstfit_location_manager(unsigned int memory_size)
+		: firstfit_manager(memory_size, compare) { };
+};
