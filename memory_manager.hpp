7df0111958255b2e67bcbd739c03ed5a0c23a945
Lucas Åström <lucasas@kth.se>
2015-07-01 11:35:59 +0200

    adjusted memory_manager api

--- memory_manager.hpp
+++ memory_manager.hpp
@@ -4,21 +4,25 @@
 
 class memory_manager
 {
+	unsigned int free_size;
+
 protected:
-	memory_manager(unsigned int memory_size) : memory_size(memory_size) { };
+	memory_manager(unsigned int memory_size) : memory_size(memory_size), free_size(memory_size) { };
+
 public:
 	struct block
 	{
 		const unsigned int start;
 		const unsigned int size;
 
+		block() : start(0), size(0) { };
 		block(unsigned int start, unsigned int size) : start(start), size(size) { };
 	};
 
 	const unsigned int memory_size;
 
 	virtual block alloc(unsigned int size) = 0;
-	virtual void free(block b) = 0;
+	virtual void free(block&& b) = 0;
 
 	using compare_function = std::function<bool(const block&, const block&)>;
 
